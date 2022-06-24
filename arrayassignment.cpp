c50a21a25a894dc83679070890e788bbda09abbc
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-06-24 17:01:36 +0500

    Update arrayassignment.cpp

--- arrayassignment.cpp
+++ arrayassignment.cpp
@@ -3,14 +3,5 @@ using namespace std;
 int main() {
     // Write C++ code here
     int arr[8]={1,2,3,4,5,4,2,4};
-    /*int max;
-    max=arr[0];
-    for (int i = 0; i < 5; i++)
-    {
-        if(arr[i]>max)
-        max=arr[i];
-    }*/
-    int sr[]={3,5,21,52,2};
-    cout<<sizeof(sr)/sizeof(sr[0]);   
-    cout<<sizeof(arr);   
+    cout<<sizeof(arr)/sizeof(arr[0]);
 }
